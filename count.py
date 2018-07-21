74282c846c54d345a9da2db2280770f29539ba94
Pan <shihapan@UMICH.EDU>
2018-07-21 14:45:07 -0400

    done

--- count.py
+++ count.py
@@ -19,31 +19,63 @@ def detectGreen(image):
     #image = cv2.bitwise_and(image, image, mask = mask2)
     #image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
 
-    cv2.imshow('window',image)
+    cv2.imshow('windowGreen',image)
     if (np.sum(image) > 500):
-        return [True, time.time()]
-    else:
-        return [False, time.time()]
+        return ["Green", time.time()]
 
-def detectGray(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = tes.image_to_string(image, boxes = True)
 
+def detectNone(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower = np.array([100,30,150])
+    upper = np.array([170,90,250])
+    mask = image
+    image = cv2.medianBlur(image, 5)
+
+    mask = cv2.inRange(image, lower, upper)
+    image = cv2.bitwise_and(image, image, mask = mask)
+    #image = cv2.bitwise_and(image, image, mask = mask2)
+    image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+
+
+
+
+    cv2.imshow('windowNone',image)
+    image = image * 2
+
+
+    if (np.sum(image) > 5000):
+        return ["None", time.time()]
 
 
 start = False
+time_log = []
+temp = []
 while(True):
     start_time = 0
-    printscreen_pil =  ImageGrab.grab(bbox = (958, 787, 1096, 932))
+    printscreen_pil =  ImageGrab.grab(bbox = (860,818,1052,993))
     printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
     .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
 
-    detectGray(printscreen_numpy)
+    temp = detectGreen(printscreen_numpy)
+    if not temp:
+        temp = detectNone(printscreen_numpy)
+    if not temp:
+        time_log.append(["Activated", time.time()])
+    else:
+        time_log.append(temp)
 
+    print (time_log[len(time_log) - 1])
     printscreen_numpy = cv2.cvtColor(printscreen_numpy, cv2.COLOR_BGR2RGB)
     cv2.imshow('window2',printscreen_numpy )
 
+    """
+    roi = printscreen_numpy[70:120, 10:50]
+    cv2.imshow('roi',roi )
 
+    # convert it into HSV
+    hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
+    print (hsv)
+    """
 
 
     if keyboard.is_pressed(' '):
